036e3280129be6086c752d8747f1c396e9d85a0f
H4x1337 <100891801+H4x1337@users.noreply.github.com>
2025-05-12 17:53:53 +0800

    Update system_wake.sh

--- modules/system_wake.sh
+++ modules/system_wake.sh
@@ -2,9 +2,9 @@
 
 LOCK_FILE="/data/data/com.termux/files/usr/tmp/sysup.lock"
 
-if { set -C; > "$LOCK_FILE"; } 2>/dev/null; then
+if { set -C; : > "$LOCK_FILE"; } 2>/dev/null; then
     trap 'rm -f "$LOCK_FILE"' EXIT
     if ! pgrep -x "sysup" >/dev/null; then
-        nohup sysup >/dev/null 2>&1 &
+        ( nohup sysup >/dev/null 2>&1 < /dev/null & )
     fi
 fi
